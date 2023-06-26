fd8271132898778596055a7ff7783e65c980df57
Yudistira Sira Permana <yudistirasirapermana@gmail.com>
2023-06-26 15:31:58 +0700

    feat : icon view sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
   <div class="menu_section">
     <ul class="nav side-menu">
-      <li><a><i class="fa fa-money"></i> Penjualan <span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-upload"></i> Penjualan <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="/penjualan">Penjualan</a></li>
           <li><a href="/">Pembelian cicilan</a></li>
@@ -10,7 +10,7 @@
           <li><a href="/">Posting jurnal penjualan admin</a></li>
         </ul>
       </li>
-      <li><a><i class="fa fa-money"></i> Pembelian <span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-download"></i> Pembelian <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="/">Data Suplier</a></li>
           <li><a href="/pembelian">Pembelian</a></li>
@@ -19,7 +19,7 @@
           <li><a href="/">Lap Retur Pembelian</a></li>
         </ul>
       </li>
-      <li><a><i class="fa fa-edit"></i> Akutansi <span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-file-text-o"></i> Akutansi <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="/">Master Akun</a></li>
           <li><a href="/">Tranksaksi Jurnal</a></li>
@@ -29,12 +29,12 @@
           <li><a href="/">PPH Terhutang</a></li>
           <li><a href="/">Trial Balance</a></li>
           <li><a href="/">Penghitungan Hasil Usaha / PHU</a></li>
-          <li><a href="">Neraca Keuangan</a></li>
+          <li><a href="/">Neraca Keuangan</a></li>
           <li><a href="/">Kertas Kerja Neraca</a></li>
           <li><a href="/">Cash Flow</a></li>
         </ul>
       </li>
-      <li><a><i class="fa fa-money"></i> Gudang <span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-cubes"></i> Gudang <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="/">Data Toko / Outlet</a></li>
           <li><a href="/">Data Barang</a></li>
@@ -45,7 +45,7 @@
           <li><a href="/">Mutasi</a></li>
         </ul>
       </li>
-      <li><a><i class="fa fa-money"></i> Anggota <span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-group"></i> Anggota <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="/">Estate/OU</a></li>
           <li><a href="/">Data Anggota</a></li>
@@ -55,22 +55,15 @@
           <li><a href="/">Pembagian SHU</a></li>
           <li><a href="/">Distribusi Pembagian SHU</a></li>
         </ul>
-      <li><a><i class="fa fa-money"></i> Pengaturan <span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-gears"></i> Pengaturan <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="/">User Account</a></li>
           <li><a href="/">Change Password</a></li>
           <li><a href="/">Log Activity</a></li>
         </ul>
       </li>
-      <li> <a href="/profiless">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="currentColor" fill-rule="evenodd"
-              d="M12 1.25a4.75 4.75 0 1 0 0 9.5a4.75 4.75 0 0 0 0-9.5ZM8.75 6a3.25 3.25 0 1 1 6.5 0a3.25 3.25 0 0 1-6.5 0ZM12 12.25c-2.313 0-4.445.526-6.024 1.414C4.42 14.54 3.25 15.866 3.25 17.5v.102c-.001 1.162-.002
-                 2.62 1.277 3.662c.629.512 1.51.877 2.7 1.117c1.192.242 2.747.369 4.773.369s3.58-.127 4.774-.369c1.19-.24 2.07-.605 2.7-1.117c1.279-1.042 1.277-2.5 1.276-3.662V17.5c0-1.634-1.17-2.96-2.725-3.836c-1.58-.888-3.711-1.414-6.025-1.414ZM4.75 17.5c0-.851.622-1.775 1.961-2.528c1.316-.74 3.184-1.222 5.29-1.222c2.104 0 3.972.482 5.288
-                1.222c1.34.753 1.961 1.677 1.961 2.528c0 1.308-.04 2.044-.724 2.6c-.37.302-.99.597-2.05.811c-1.057.214-2.502.339-4.476.339c-1.974 0-3.42-.125-4.476-.339c-1.06-.214-1.68-.509-2.05-.81c-.684-.557-.724-1.293-.724-2.601Z"
-              clip-rule="evenodd" />
-          </svg>
-          </i> Profile</a>
+      <li><a href="/profiless"><i class="fa fa-user"></i> Profile <span class="fa fa-chevron-down"></span></a>
+
       </li>
     </ul>
   </div>
