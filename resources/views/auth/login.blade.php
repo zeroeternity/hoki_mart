02859cdc6184d4949f41e492782b2bc8d8723bb1
Yudistira Sira Permana <yudistirasirapermana@gmail.com>
2023-07-21 10:24:56 +0700

    fix : icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="{{ asset('images/hoki.ico') }}">
+  <link rel="icon" type="image/x-icon" href="{{ asset('images/hoki-ico.png') }}">
 
 
   <title>Hoki Mart </title>
