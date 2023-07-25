a548e30fdccbcbdf7c753819d8d0a57ff83a7104
Yudistira Sira Permana <yudistirasirapermana@gmail.com>
2023-07-25 17:57:34 +0700

    feat : domPDF sales transaction

---
+++ resources/views/page/sale/print.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style >
+        @page  {
+            margin-top: 5px;
+            margin-left: 5px;
+            margin-right: 5px;
+        }
+        h1{
+            font-family: "Lucida Console";
+            font-size: 5px;
+        }
+        p{
+            font-family: "Lucida Console";
+            font-size: 4px;
+        }
+    </style>
+    <title>Cetak Struk</title>
+
+</head>
+<body>
+<h1>Struk Title</h1>
+<p>-------------------------------------------------------------------</p>
+<p>Invoice Number: </p>
+<p>Customer Name: </p>
+<!-- Add more struk details here -->
+</body>
+</html>
