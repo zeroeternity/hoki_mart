9f8af9ed68ab64ddf7d0243339e42b1cd8a83e00
Yudistira Sira Permana <yudistirasirapermana@gmail.com>
2023-07-31 00:28:16 +0700

    feat : dummy line chart dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('page.dashboard.dashboard');
+        $dataSeries1 = [
+            'label' => 'Data Pembelian',
+            'data' => [10, 20, 15, 30, 25],
+            'backgroundColor' => 'rgba(75, 192, 192, 0.2)',
+            'borderColor' => 'rgba(75, 192, 192, 1)',
+            'borderWidth' => 1,
+        ];
+
+        $dataSeries2 = [
+            'label' => 'Data Penjualan',
+            'data' => [5, 15, 8, 12, 20],
+            'backgroundColor' => 'rgba(255, 159, 64, 0.2)',
+            'borderColor' => 'rgba(255, 159, 64, 1)',
+            'borderWidth' => 1,
+        ];
+
+        $chartData = [
+            'labels' => ['January', 'February', 'March', 'April', 'May'],
+            'datasets' => [$dataSeries1, $dataSeries2],
+        ];
+
+        return view('page.dashboard.dashboard')->with('chartData', $chartData);
     }
 }
--- resources/views/page/dashboard/dashboard.blade.php
+++ resources/views/page/dashboard/dashboard.blade.php
@@ -8,10 +8,35 @@
                 <h3>Dashboard</h3>
             </div>
         </div>
+        <canvas id="myChart" height="100px"></canvas>
         <div class="clearfix"></div>
 
     </div>
 </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const chartData = @json($chartData);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    let myChart;
+
+    function createChart(data) {
+        if (myChart) {
+            myChart.destroy();
+        }
+        myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: data.labels,
+                datasets: data.datasets,
+            },
+            options: {
+            }
+        });
+    }
+    createChart(chartData);
+</script>
+
 
 @endsection
